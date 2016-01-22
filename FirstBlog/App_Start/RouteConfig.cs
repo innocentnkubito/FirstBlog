696f88d8b25d9356442090582f4cff19f6d152b1
Innocent Nkubito <Innocent.Nkubito@eagles.oc.edu>
2016-01-22 06:02:19 -0600

    Added routers, controllers and areas.

--- FirstBlog/App_Start/RouteConfig.cs
+++ FirstBlog/App_Start/RouteConfig.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
+using FirstBlog.Controllers;
 
 namespace FirstBlog
 {
@@ -11,14 +12,13 @@ namespace FirstBlog
     {
         public static void RegisterRoutes(RouteCollection routes)
         {
+            var namespaces = new[] { typeof(PostsController).Namespace };
+
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
-           /* routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-            ); */
-            routes.MapRoute("Home", " ", new { controller = "Posts", action = "Index" });
+            routes.MapRoute("Login", "login", new { controller = "Auth", action = "Login" }, namespaces);
+
+            routes.MapRoute("Home", "", new { controller = "Posts", action = "Index" },namespaces );
         }
     }
 }
