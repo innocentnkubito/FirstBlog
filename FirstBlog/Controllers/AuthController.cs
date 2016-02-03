5f42d266819b850f46a93b167027ee34d0c00868
Innocent Nkubito <Innocent.Nkubito@eagles.oc.edu>
2016-02-03 07:02:37 -0600

    Added basic login form

--- FirstBlog/Controllers/AuthController.cs
+++ FirstBlog/Controllers/AuthController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using FirstBlog.ViewModels;
 
 namespace FirstBlog.Controllers
 {
@@ -15,6 +16,22 @@ namespace FirstBlog.Controllers
         {
             return View();
         }
+
+        [HttpPost]
+        public ActionResult Login(AuthLogin form)
+       {
+           if (!ModelState.IsValid)
+               return View(form);
+
+            if(form.Username != "ranbow")
+            {
+                ModelState.AddModelError("Username", "Username or password is unknown");
+                return View(form);
+            }
+
+           
+           return Content("It is valid");
+       }
 
     }
 }
---
+++ FirstBlog/ViewModels/Auth.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace FirstBlog.ViewModels
+{
+    public class AuthLogin
+    {
+        [Required]
+        public string Username { get; set; }
+
+        [Required, DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
